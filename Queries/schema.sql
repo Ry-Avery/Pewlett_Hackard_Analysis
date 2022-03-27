51b0d13e746a1622ac6dc723d120b9aec23c59da
Ry Avery <axeguy255@gmail.com>
2022-03-26 17:20:06 -0700

    Data analysis
    Used pgAdmin queries to create new csv datasets in my Data folder

--- Queries/schema.sql
+++ Queries/schema.sql
@@ -21,7 +21,7 @@ CREATE TABLE dept_manager(
 	to_date DATE NOT NULL,
 	FOREIGN KEY(emp_no) REFERENCES employees(emp_no),
 	FOREIGN KEY(dept_no) REFERENCES departments(dept_no),
-	PRIMARY KEY(dept_no,emp_no)
+	PRIMARY KEY(dept_no, emp_no)
 );
 CREATE TABLE salaries(
 	emp_no INT NOT NULL,
@@ -46,6 +46,7 @@ CREATE TABLE titles(
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY(emp_no) REFERENCES employees(emp_no),
-	PRIMARY KEY(emp_no,title,from_date)
+	PRIMARY KEY(emp_no, title, from_date)
 );
-SELECT * FROM departments;
+SELECT * FROM departments
+;
